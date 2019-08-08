813f422f17eb82bc609bf0b083b7b1ea3ed207ab
psheemas <psheemas@gmail.com>
2019-08-08 18:26:50 +0200

    24.5 Without import

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,16 +1,14 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
-import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> booksFromLibraryA = new HashMap<>();
 
         bookSet.stream()
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/company/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/company/MedianAdapterTestSuite.java
@@ -1,7 +1,6 @@
 package com.kodilla.patterns2.adapter.company;
 
 import com.kodilla.patterns2.adapter.bookclasifier.MedianAdapter;
-import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import org.junit.Test;
 
 import java.util.HashSet;
@@ -13,14 +12,14 @@ public class MedianAdapterTestSuite {
     @Test
     public void publicationYearMedianTest(){
         //Given
-        Set<Book> booksFromLibraryA= new HashSet<>();
+        Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> booksFromLibraryA= new HashSet<>();
         MedianAdapter adapterToCalculate = new MedianAdapter();
-        booksFromLibraryA.add(new Book("Steven","StevenTitle",1990,"1zjjdeiigo"));
-        booksFromLibraryA.add(new Book("Martin","MartinTitle",1991,"1zj453eiigo"));
-        booksFromLibraryA.add(new Book("John","JohnTitle",1992,"1zjjdeloioio"));
-        booksFromLibraryA.add(new Book("Tom","TomTitle",1993,"1zjjdezzsso"));
-        booksFromLibraryA.add(new Book("Martha","MarthaTitle",1996,"1zjjdezzsso"));
-        booksFromLibraryA.add(new Book("Ivonne","IvonneTitle",2000,"1zjjdezzsso"));
+        booksFromLibraryA.add(new com.kodilla.patterns2.adapter.bookclasifier.librarya.Book("Steven","StevenTitle",1990,"1zjjdeiigo"));
+        booksFromLibraryA.add(new com.kodilla.patterns2.adapter.bookclasifier.librarya.Book("Martin","MartinTitle",1991,"1zj453eiigo"));
+        booksFromLibraryA.add(new com.kodilla.patterns2.adapter.bookclasifier.librarya.Book("John","JohnTitle",1992,"1zjjdeloioio"));
+        booksFromLibraryA.add(new com.kodilla.patterns2.adapter.bookclasifier.librarya.Book("Tom","TomTitle",1993,"1zjjdezzsso"));
+        booksFromLibraryA.add(new com.kodilla.patterns2.adapter.bookclasifier.librarya.Book("Martha","MarthaTitle",1996,"1zjjdezzsso"));
+        booksFromLibraryA.add(new com.kodilla.patterns2.adapter.bookclasifier.librarya.Book("Ivonne","IvonneTitle",2000,"1zjjdezzsso"));
         //When
         int result = adapterToCalculate.publicationYearMedian(booksFromLibraryA);
         System.out.println(result);
